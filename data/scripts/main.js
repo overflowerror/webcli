ee0f7fc44c78034da9fcb7343142cf73d6d24dd9
overflowerror <mail@overflowerror.com>
2014-07-03 15:24:19 +0200

    initializing shell

--- data/scripts/main.js
+++ data/scripts/main.js
@@ -3,4 +3,10 @@ window.onload = function() {
 	terminal.calculateSize(window.innerWidth, window.innerHeight);
 	document.body.innerHTML += terminal.getBasicHTML();
 	terminal.init();
+
+	window.shell = new Shell();
+	shell.output = function(text) { 
+		window.terminal.output(text);
+	};
+	shell.main([]);
 }
