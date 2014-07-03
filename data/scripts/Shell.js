eb342789f4fecb18925cc75b947578ce4513abef
overflowerror <mail@overflowerror.com>
2014-07-03 15:46:38 +0200

    added prompt; no input yet

--- data/scripts/Shell.js
+++ data/scripts/Shell.js
@@ -6,6 +6,10 @@ ProgramManager.programs.push({
 var Shell = function() {
 }
 Shell.prototype = new Program();
+Shell.prototype.prompt = "\033[36m\\t\033[0m \033[35m\\h\033[0m\033[1m:\033[0m\\w \\$ ";
+Shell.prototype.user = "nobody";
+Shell.prototype.hostname = "webcli";
+Shell.prototype.directory = "/";
 Shell.prototype.centeredOutput = function(text) { // does not work for ansi.sequences
 	var line = "";
 	for (var i = 0; i < text.length; i++) {
@@ -45,7 +49,22 @@ _(())\\_)()((_)((_)_  )\\___ \033[33m(_)\033[31m)  (_))   \n\
 	this.centeredOutput("\
 Welcome to Webcli\n\
 =================");
-	
+	this.output("\n\n\033[0mYou are logged in as \"nobody\". Use `su` to change your user.\n\033[31mNo directory, logging in with HOME=/\033[0m\n\n");
+	this.displayPrompt();
 }
-
+Shell.prototype.displayPrompt = function() {
+	var text = this.prompt;
+	console.dir(this);
+	while (text.indexOf("\\u") != -1)
+		text = text.replace("\\u", this.user);
+	while (text.indexOf("\\h") != -1)
+		text = text.replace("\\h", this.hostname);
+	while (text.indexOf("\\w") != -1)
+		text = text.replace("\\w", this.directory);
+	while (text.indexOf("\\t") != -1)
+		text = text.replace("\\t", new Date().getHours() + ":" + new Date().getMinutes());
+	while (text.indexOf("\\$") != -1)
+		text = text.replace("\\$", (this.user == "root" ? "#" : "$"));
+	this.output(text);
+}
 
