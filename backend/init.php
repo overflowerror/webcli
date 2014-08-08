e5f92df02ac60afadf8493b07b45f59b3780a490
overflowerror <mail@overflowerror.com>
2014-08-08 02:04:22 +0200

    we need a hostname file

---
+++ backend/init.php
@@ -0,0 +1,14 @@
+<?php
+	require_once("actionLogger.php");
+	require_once("sessionManager.php");
+	require_once("userManager.php");
+	require_once("fileManager.php");
+
+	$result = array();
+	$result['uid'] = $_SESSION['uid'];
+	$user = userManager::getUserById($_SESSION['uid']);
+	$result['username'] = $user->name;
+	$result['home'] = fileManager::getPathById($user->homeFK);
+	$result['hostname'] = fileManager::getFileById(fileManager::getIdByPath("/etc/hostname"))->content;
+	echo json_encode($result);
+?>
