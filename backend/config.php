b6b93253c8c94dca64efb984f68f103ca9156fda
overflowerror <mail@overflowerror.com>
2014-08-17 18:13:26 +0200

    added mysql credentials to config.php

--- backend/config.php
+++ backend/config.php
@@ -3,9 +3,16 @@
 
 	define("LOG_FILE",		"/var/www/websites/webcli/internal.log");
 	
-	define("DEFAULT_USER",		3);
+	define("DB_HOST",		"localhost");
+	define("DB_USER",		"webcli");
+	define("DB_PASSWORD",		"password");
+	define("DB_NAME",		"webcli");
+
+	define("DEFAULT_USER",		3); 		// default value: 3 (nobody)
 	define("DEFAULT_HOSTNAME", 	"webcli");
 	define("SIZE_OF_DIRECTORY",	4096);
+
+	// do not change anything below here
 
 	define("FILE_FLAG_EXECUTE",	1 << 0);
 	define("FILE_FLAG_WRITE",	1 << 1);	
--- backend/mysqlConnect.php
+++ backend/mysqlConnect.php
@@ -1,5 +1,7 @@
 <?php
-	$db = new mysqli("localhost", "webcli", "password", "webcli");
+	require_once("config.php");
+
+	$db = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 	if ($db->connect_error)
 		die("Connect Error (" . $db->connect_errno . ") " . $db->connect_error);
 ?>
